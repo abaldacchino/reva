0d597586896a6bb66bdf35fba4a1c34d6d87029f
Adriana Baldacchino <adriana.baldacchino@cern.ch>
2023-08-04 11:41:13 +0200

    Initial shell and logic of overleaf import provider

--- pkg/app/provider/overleafimport/overleafimport.go
+++ pkg/app/provider/overleafimport/overleafimport.go
@@ -1,1 +1,127 @@
+// Copyright 2018-2023 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 package overleafimport
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	appprovider "github.com/cs3org/go-cs3apis/cs3/app/provider/v1beta1"
+	appregistry "github.com/cs3org/go-cs3apis/cs3/app/registry/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	typespb "github.com/cs3org/go-cs3apis/cs3/types/v1beta1"
+	"github.com/cs3org/reva/pkg/app"
+	"github.com/cs3org/reva/pkg/app/provider/registry"
+	"github.com/cs3org/reva/pkg/rgrpc/todo/pool"
+	"github.com/cs3org/reva/pkg/rhttp"
+	"github.com/cs3org/reva/pkg/sharedconf"
+	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
+)
+
+type overleafImportProvider struct {
+	conf           *config
+	overleafClient *http.Client
+}
+
+func (p *overleafImportProvider) GetAppURL(ctx context.Context, resource *provider.ResourceInfo, viewMode appprovider.ViewMode, token string, opaqueMap map[string]*typespb.OpaqueEntry, language string) (*appprovider.OpenInAppURL, error) {
+	// log := appctx.GetLogger(ctx)
+
+	client, err := pool.GetGatewayServiceClient(pool.Endpoint(sharedconf.GetGatewaySVC("")))
+	if err != nil {
+		return nil, errors.Wrap(err, "overleaf: error fetching gateway service client.")
+	}
+
+	// Resource must be already Imported to be imported again
+	statRes, err := client.Stat(ctx, &provider.StatRequest{
+		Ref: &provider.Reference{
+			ResourceId: resource.Id,
+		},
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "overleaf: error statting file.")
+	}
+
+	_, found := statRes.Info.GetArbitraryMetadata().Metadata["reva.overleaf.time"]
+	if !found {
+		return nil, errors.New("overleaf import: file not previously exported, error getting file export time")
+	}
+	// name, found := statRes.Info.GetArbitraryMetadata().Metadata["reva.overleaf.name"]
+	// if !found {
+	// 	return nil, errors.New("overleaf import: error getting file export name")
+	// }
+
+	return &appprovider.OpenInAppURL{
+		AppUrl: "",
+		Method: http.MethodGet,
+		Target: appprovider.Target_TARGET_BLANK,
+	}, nil
+}
+
+func (p *overleafImportProvider) GetAppProviderInfo(ctx context.Context) (*appregistry.ProviderInfo, error) {
+	return &appregistry.ProviderInfo{
+		Name:      "Overleaf",
+		MimeTypes: p.conf.MimeTypes,
+		Icon:      p.conf.AppIconURI,
+		Action:    "Import from",
+	}, nil
+}
+
+func init() {
+	registry.Register("overleaf", New)
+}
+
+type config struct {
+	MimeTypes           []string `mapstructure:"mime_types" docs:"nil;Inherited from the appprovider."`
+	AppName             string   `mapstructure:"app_name" docs:";The App user-friendly name."`
+	AppIconURI          string   `mapstructure:"app_icon_uri" docs:";A URI to a static asset which represents the app icon."`
+	AppURL              string   `mapstructure:"app_url" docs:";The App URL."`
+	AppIntURL           string   `mapstructure:"app_int_url" docs:";The internal app URL in case of dockerized deployments. Defaults to AppURL"`
+	InsecureConnections bool     `mapstructure:"insecure_connections"`
+	Cookie              string   `mapstructure:"cookie" docs:"Stores user cookie to access files as a temporary workaround"`
+}
+
+func parseConfig(m map[string]interface{}) (*config, error) {
+	c := &config{}
+	if err := mapstructure.Decode(m, c); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// New returns an implementation to of the app.Provider interface that
+// connects to an application in the backend.
+func New(ctx context.Context, m map[string]interface{}) (app.Provider, error) {
+	c, err := parseConfig(m)
+	if err != nil {
+		return nil, err
+	}
+
+	// Need http client to make requests to Overleaf server
+	overleafClient := rhttp.GetHTTPClient(
+		rhttp.Timeout(time.Duration(10*int64(time.Second))),
+		rhttp.Insecure(c.InsecureConnections),
+	)
+	overleafClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	return &overleafImportProvider{conf: c, overleafClient: overleafClient}, nil
+}
