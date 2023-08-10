aac6ba549a601d92ada97ec06fa4736e1e4194f9
Adriana Baldacchino <adriana.baldacchino@cern.ch>
2023-08-10 15:59:04 +0200

    Adapted Overleaf New fn and removed from loaders

--- pkg/app/provider/loader/loader.go
+++ pkg/app/provider/loader/loader.go
@@ -21,8 +21,6 @@ package loader
 import (
 	// Importing app providers.
 	_ "github.com/cs3org/reva/pkg/app/provider/demo"
-	_ "github.com/cs3org/reva/pkg/app/provider/overleaf"
-	_ "github.com/cs3org/reva/pkg/app/provider/overleafimport"
 	_ "github.com/cs3org/reva/pkg/app/provider/wopi"
 	// Add your own here.
 )
